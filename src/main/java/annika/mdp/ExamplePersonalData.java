aeaea3fb2e81358a7c2a197fab98c066539b65b3
Annika <annika.oesterdiekhoff@ewe.net>
2020-09-15 18:14:22 +0200

    example personal data

---
+++ src/main/java/annika/mdp/ExamplePersonalData.java
@@ -0,0 +1,11 @@
+package annika.mdp;
+
+public class ExamplePersonalData {
+	public String outputFile = "path/where/outputFile/should/be";
+	public String inputFileLeiterrunde = "path/to/leiterrunde.csv";
+	public String inputFileMessdiener = "path/to/messdiener.csv";
+	public String name = "YOURNAME";
+	public String street = "YOURSTREET"; 
+	public String plz = "YOURPLZ+TOWN";
+	public String mailadress = "YOURMAILADRESS";
+}
